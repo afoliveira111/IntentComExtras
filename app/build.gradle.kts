5bdef5011d904b4ed067ca0078513f26d4879388
Felipe <felipetermometria@hotmail.com>
2023-12-01 17:57:21 -0300

    Sync gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.intentcomextras"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.intentcomextras"
