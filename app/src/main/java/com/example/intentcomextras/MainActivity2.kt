e0b7a1a711c63d36f668850d105a1582ae407363
Felipe <felipetermometria@hotmail.com>
2023-12-01 09:43:22 -0300

    viewBinding

--- app/src/main/java/com/example/intentcomextras/MainActivity2.kt
+++ app/src/main/java/com/example/intentcomextras/MainActivity2.kt
@@ -2,10 +2,23 @@ package com.example.intentcomextras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.intentcomextras.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val i = intent
+        val nome = i.extras?.getString("nome")
+
+        if(nome.equals("") || nome == null){
+            Toast.makeText(applicationContext, "Nome não inserido", Toast.LENGTH_SHORT).show()
+        }
     }
 }
